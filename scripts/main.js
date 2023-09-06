ea2be6cd1ae33a363e88c8233838abd4882d53ad
Mpho Lebona <giftedmpho99@gmail.com>
2023-09-06 15:28:28 +0200

    New Navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-// Get dropdown content
+// Get the button and dropdown content
 const dropdownButton = document.querySelector(".dropdown-btn");
 const dropdownContent = document.querySelector(".dropdown-content");
 
 // Add a click event listener to the button
-dropdownButton.addEventListener("mouseover", function () {
+dropdownButton.addEventListener("click", function () {
   // Toggle the display style of the dropdown content
   if (dropdownContent.style.display === "block") {
     dropdownContent.style.display = "none";
