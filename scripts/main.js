44413f15da28f89968709089085452db395455c4
Mpho Lebona <giftedmpho99@gmail.com>
2023-09-06 13:38:20 +0200

    Draft

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// Get dropdown content
+const dropdownButton = document.querySelector(".dropdown-btn");
+const dropdownContent = document.querySelector(".dropdown-content");
+
+// Add a click event listener to the button
+dropdownButton.addEventListener("mouseover", function () {
+  // Toggle the display style of the dropdown content
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+});
