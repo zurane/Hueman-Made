d5d6c838e228c4d8291621dac838c62c84e4b8f4
Mpho Lebona <giftedmpho99@gmail.com>
2023-09-07 17:05:46 +0200

    Afternoon

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
+
+// This is a simple toggole function for the dropdown menu
 // Get dropdown content
 const dropdownButton = document.querySelector(".dropdown-btn");
 const dropdownContent = document.querySelector(".dropdown-content");
 const icon = document.querySelector(".material-symbols-outlined");
 
-
 dropdownButton.addEventListener("click", function () {
-
 
   if (dropdownContent.style.display === "inline-flex") {
     icon.innerHTML = `<span class="material-symbols-outlined">expand_more</span>`
