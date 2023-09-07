8cf3b6c774a973c9fcf10917126179cdea9b915c
zurane <giftedmpho99@gmail.com>
2023-09-07 06:53:00 +0200

    Sun

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,18 @@
 // Get dropdown content
 const dropdownButton = document.querySelector(".dropdown-btn");
 const dropdownContent = document.querySelector(".dropdown-content");
+const icon = document.querySelector(".material-symbols-outlined");
 
 
 dropdownButton.addEventListener("click", function () {
 
 
   if (dropdownContent.style.display === "inline-flex") {
+    icon.innerHTML = `<span class="material-symbols-outlined">expand_more</span>`
     dropdownContent.style.display = "none";
   } else {
+
+    icon.innerHTML = `<span class="material-symbols-outlined">expand_less</span>`
     dropdownContent.style.display = "inline-flex";
   }
 
