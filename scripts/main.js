c746e67f58238f9f399c9500756693f0cc9b35d8
Mpho Lebona <giftedmpho99@gmail.com>
2023-09-12 15:11:55 +0200

    commited

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,32 @@
-
 // This is a simple toggole function for the dropdown menu
 // Get dropdown content
-const dropdownButton = document.querySelector(".dropdown-btn");
+
 const dropdownContent = document.querySelector(".dropdown-content");
-const icon = document.querySelector(".material-symbols-outlined");
+const icons = document.querySelectorAll(".menu-icon");
+let anchorTags = `<a href='https://www.google.com'>Nothing</a>`;
 
-dropdownButton.addEventListener("click", function () {
+document.querySelectorAll(".dropdown-btn").forEach((item) => {
+  item.addEventListener("click", HandleClick);
+});
 
+// Every evenListener takes a callBack function as it's second parameter and rather the nature of how it should react should be all encased within your callback function.
+function HandleClick() {
   if (dropdownContent.style.display === "inline-flex") {
-    icon.innerHTML = `<span class="material-symbols-outlined">expand_more</span>`
     dropdownContent.style.display = "none";
   } else {
-
-    icon.innerHTML = `<span class="material-symbols-outlined">expand_less</span>`
+    dropdownContent.innerHTML = anchorTags;
     dropdownContent.style.display = "inline-flex";
   }
+}
 
-})
+// Remember that if you want to affect an elements with similar properties or property, you iterate using the forEach loop.
+// Ater it iterates through all the element i will apply an event listener on each element and in this case toggling between two different icons.
+icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    if (icon.textContent === "expand_more") {
+      return (icon.textContent = "expand_less");
+    } else {
+      return (icon.textContent = "expand_more");
+    }
+  });
+});
