ecb72c984e98c4977df106a83fd5072de58deab0
Mpho Lebona <giftedmpho99@gmail.com>
2023-09-14 16:30:05 +0200

    Added sliders and function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 // This is a simple toggole function for the dropdown menu
 // Get dropdown content
-
+const carousel = document.querySelector('.carousel');
+const carouselArrows = document.querySelectorAll('.carousel-arrow');
+const firstImg = carousel.querySelectorAll('.img')[0];
 const dropdownContent = document.querySelector(".dropdown-content");
 const icons = document.querySelectorAll(".menu-icon");
 let anchorTags = `<a href='https://www.google.com'>Nothing</a>`;
@@ -30,3 +32,41 @@ icons.forEach((icon) => {
     }
   });
 });
+
+
+// Carousel section script 
+
+let isDragStart = false, prevPageX, prevScrollleft;
+
+let firstImgWidth = firstImg.clientWidth + 14; // getting the first image and adding the first 14px margin value
+
+carouselArrows.forEach(icon => {
+  icon.addEventListener('click', () => {
+    // this ternary function reduces the width of the first image when the left icon is clicked or otherwise add it.
+        carousel.scrollLeft += icon.id === 'left' ? -firstImgWidth : firstImgWidth
+    })
+})
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX;
+  prevScrollleft = carousel.pageX;
+}
+
+const dragging = (e) => {
+
+  // Remember an exclamation mark is known as the "logical NOT" operator. It negates the truthiness of a value. 
+  // In other words, it flips a true value to false and a false value to true. For example:
+  if (!isDragStart) return;
+  e.preventDefault()
+  let positionDiff = e.pageX - prevPageX
+  carousel.scrollLeft = prevScrollleft - positionDiff;
+}
+
+const dragStop = () => {
+  isDragStart = false;
+}
+
+carousel.addEventListener('mousedown', dragStart)
+carousel.addEventListener('mousemove', dragging)
+carousel.addEventListener('mouseover', dragStop)
